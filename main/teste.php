688d965612139c1d027dc674291bfbdd4103e5c9
Elai <emyllematos7@gmail.com>
2018-07-20 11:53:59 -0300

    Temp
    123456

---
+++ main/teste.php
@@ -0,0 +1,18 @@
+<?php
+  $q = $_REQUEST["q"];
+  $hint = "";
+
+  if ($q !== "") {
+    $q = strtolower($q);
+    $len=strlen($q);
+    foreach($a as $name) {
+        if (stristr($q, substr($name, 0, $len))) {
+            if ($hint === "") {
+                $hint = $name;
+            } else {
+                $hint .= ", $name";
+            }
+"color:red">         }
+
+  echo $hint === "" ? "no suggestion" : $hint;
+?>

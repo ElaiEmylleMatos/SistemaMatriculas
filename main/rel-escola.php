8f8eb2bc44eba912d28ed2289941091a391601d4
Elai <emyllematos7@gmail.com>
2018-07-26 14:42:49 -0300

    123456

--- main/rel-escola.php
+++ main/rel-escola.php
@@ -82,6 +82,13 @@
             </div>
         </aside>
         <!-- ============================================================== -->
+        <?php
+          include '../model/conexao.php';
+          $sql = "select * from escolas";
+          $res = mysqli_query($link,$sql);
+
+        ?>
+        <!-- ============================================================== -->
         <div class="page-wrapper">
             <div class="container-fluid">
                 <br>
@@ -91,84 +98,28 @@
                           <div class="card-body">
                               <div class="d-flex">
                                   <div>
-                                      <h5 class="card-title"><?php echo "Sales Overview"?></h5>
-                                      <h6 class="card-subtitle">Check the monthly sales </h6>
-                                  </div>
-                                  <div class="ml-auto">
-                                      <select class="custom-select b-0">
-                                          <option>January</option>
-                                          <option value="1">February</option>
-                                          <option value="2" selected="">March</option>
-                                          <option value="3">April</option>
-                                      </select>
+                                      <h1>Relatório de Escolas</h1>
+                                      <p class="text-muted m-b-30 font-13"> Todas as escolas cadastradas.</p>
                                   </div>
                               </div>
                           </div>
                           <div class="table-responsive">
-                              <table class="table table-hover">
+                              <table class="table table-hover" style="background-color:#fff;">
                                   <thead>
                                       <tr>
-                                          <th class="text-center">#</th>
-                                          <th>NAME</th>
-                                          <th>DATE</th>
-                                          <th>PRICE</th>
+                                          <th>Nome da Escola</th>
+                                          <th>Data de Cadastro</th>
                                       </tr>
                                   </thead>
                                   <tbody>
-                                      <tr>
-                                          <td class="text-center">1</td>
-                                          <td class="txt-oflo">Elite admin</td>
-                                          <td class="txt-oflo">April 18, 2017</td>
-                                          <td><span class="text-success">$24</span></td>
-                                      </tr>
-                                      <tr>
-                                          <td class="text-center">2</td>
-                                          <td class="txt-oflo">Real Homes WP Theme</td>
-                                          <td class="txt-oflo">April 19, 2017</td>
-                                          <td><span class="text-info">$1250</span></td>
-                                      </tr>
-                                      <tr>
-                                          <td class="text-center">3</td>
-                                          <td class="txt-oflo">Ample Admin</td>
-                                          <td class="txt-oflo">April 19, 2017</td>
-                                          <td><span class="text-info">$1250</span></td>
-                                      </tr>
-                                      <tr>
-                                          <td class="text-center">4</td>
-                                          <td class="txt-oflo">Medical Pro WP Theme</td>
-                                          <td class="txt-oflo">April 20, 2017</td>
-                                          <td><span class="text-danger">-$24</span></td>
-                                      </tr>
-                                      <tr>
-                                          <td class="text-center">5</td>
-                                          <td class="txt-oflo">Hosting press html</td>
-                                          <td class="txt-oflo">April 21, 2017</td>
-                                          <td><span class="text-success">$24</span></td>
-                                      </tr>
-                                      <tr>
-                                          <td class="text-center">6</td>
-                                          <td class="txt-oflo">Digital Agency PSD</td>
-                                          <td class="txt-oflo">April 23, 2017</td>
-                                          <td><span class="text-danger">-$14</span></td>
-                                      </tr>
-                                      <tr>
-                                          <td class="text-center">7</td>
-                                          <td class="txt-oflo">Helping Hands WP Theme</td>
-                                          <td class="txt-oflo">April 22, 2017</td>
-                                          <td><span class="text-success">$64</span></td>
-                                      </tr>
-                                      <tr>
-                                          <td class="text-center">8</td>
-                                          <td class="txt-oflo">Helping Hands WP Theme</td>
-                                          <td class="txt-oflo">April 22, 2017</td>
-                                          <td><span class="text-success">$64</span></td>
-                                      </tr>
-                                      <tr>
-                                          <td class="text-center">9</td>
-                                          <td class="txt-oflo">Ample Admin</td>
-                                          <td class="txt-oflo">April 19, 2017</td>
-                                          <td><span class="text-info">$1250</span></td>
-                                      </tr>
+                                    <?php
+                                      while($row = mysqli_fetch_assoc($res)):
+                                        echo "<tr>
+                                            <td class='txt-oflo'>".$row['nome_escolas']."</td>
+                                            <td class='txt-oflo'>".$row['data_cadastro']."</td>
+                                        </tr>";
+                                      endwhile;
+                                    ?>
                                   </tbody>
                               </table>
                           </div>

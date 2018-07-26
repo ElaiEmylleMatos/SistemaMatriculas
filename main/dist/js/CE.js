bb44282b61f209c4e5247cff1a3e9abf3f5b19d9
Elai <emyllematos7@gmail.com>
2018-07-26 12:24:26 -0300

    123456

--- main/dist/js/CE.js
+++ main/dist/js/CE.js
@@ -1,6 +1,6 @@
 $(function() {
   $('#cnpjEsc').mask('00.000.000/0000-00', {reverse: true});
-  $('#cepEsc').mask('00000-000');
+  $('#cepEsc').mask('00000-000', {reverse: true});
 
   var SPMaskBehavior = function (val) {
     return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
@@ -14,7 +14,6 @@ $(function() {
   $('#telEsc').mask(SPMaskBehavior, spOptions);
 
     function limpa_formulário_cep() {
-        // Limpa valores do formulário de cep.
         $("#logradouroEsc").val("");
         $("#bairroEsc").val("");
         $("#cidadeEsc").val("");
@@ -25,43 +24,35 @@ $(function() {
     //Quando o campo cep perde o foco.
     $("#cepEsc").blur(function() {
 
-        //Nova variável "cep" somente com dígitos.
         var cep = $(this).val().replace(/\D/g, '');
 
-        //Verifica se campo cep possui valor informado.
         if (cep != "") {
 
-            //Expressão regular para validar o CEP.
             var validacep = /^[0-9]{8}$/;
 
             //Valida o formato do CEP.
             if(validacep.test(cep)) {
 
-                //Consulta o webservice viacep.com.br/
                 $.getJSON("https://viacep.com.br/ws/"+ cep +"/json/?callback=?", function(dados) {
 
                     if (!("erro" in dados)) {
-                        //Atualiza os campos com os valores da consulta.
-                        $("#logradouroEsc").val("");
-                        $("#bairroEsc").val("");
-                        $("#cidadeEsc").val("");
-                        $("#ufEsc").val("");
+                        $("#logradouroEsc").val(dados.logradouro);
+                        $("#bairroEsc").val(dados.bairro);
+                        $("#cidadeEsc").val(dados.localidade);
+                        $("#ufEsc").val(dados.uf);
                     } //end if.
                     else {
-                        //CEP pesquisado não foi encontrado.
                         limpa_formulário_cep();
                         alert("CEP não encontrado.");
                     }
                 });
             } //end if.
             else {
-                //cep é inválido.
                 limpa_formulário_cep();
                 alert("Formato de CEP inválido.");
             }
         } //end if.
         else {
-            //cep sem valor, limpa formulário.
             limpa_formulário_cep();
         }
     });

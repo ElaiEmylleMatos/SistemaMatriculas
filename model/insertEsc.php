47c3ec9a080fedaf0499fe138c96c007c4ea6075
Elai <emyllematos7@gmail.com>
2018-07-26 13:13:17 -0300

    123456

--- model/insertEsc.php
+++ model/insertEsc.php
@@ -1,19 +1,19 @@
 <?php
 
   include 'conexao.php';
-  $email = $_POST['emailEsc'];
-  $senha = $_POST['senhaEsc'];
-  $bairro = $_POST['bairroEsc'];
-  $rua = $_POST['logradouroEsc'];
-  $user = $_POST['userEsc'];
-  $nome = $_POST['nomeEsc'];
-  $tel = $_POST['telEsc'];
-  $cnpj = $_POST['cnpjEsc'];
+  $email = $_POST["emailEsc"];
+  $senha = $_POST["senhaEsc"];
+  $bairro = $_POST["bairroEsc"];
+  $rua = $_POST["logradouroEsc"];
+  $user = $_POST["userEsc"];
+  $nome = $_POST["nomeEsc"];
+  $tel = $_POST["telEsc"];
+  $cnpj = $_POST["cnpjEsc"];
   $codu = 2;
 
   $sql = "INSERT INTO escolas (email_escolas, senha_escolas, bairro_escolas, rua_escolas, cod_escolas, nomeAcesso_escolas, nome_escolas, telefone_escolas, cnpj, cod_users) VALUES ,('$email', '$senha', '$bairro', '$rua', NULL, '$user', '$nome', '$tel', '$cnpj', '$codu')";
 
-  $res = mysqli_query($link,$ins);
+  $res = mysqli_query($link,$sql);
 
 	if ($res) {
 		header("location:../main/rel-est.html");

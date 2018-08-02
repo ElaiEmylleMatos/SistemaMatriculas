0ee7c654d4b2768f1b59ef4166cafec11b0e3128
Elai <emyllematos7@gmail.com>
2018-08-02 15:16:25 -0300

    123456

--- main/open.php
+++ main/open.php
@@ -8,10 +8,12 @@
 	  $senha = $_POST['senha'];
 
 		$sql = "SELECT nomeAcesso_escolas, senha_escolas FROM escolas WHERE nomeAcesso_escolas = '$user' AND senha_escolas = '$senha'";
+		$adm = "SELECT nomeAcesso_adm, senha_adm FROM adm WHERE nomeAcesso_adm = '$user' AND senha_adm = '$senha'";
 
 	  $resultado = mysqli_query($link, $sql);
+		$fim = mysqli_query($link, $adm);
 
-	  if(mysqli_num_rows($resultado) > 0){
+		if(mysqli_num_rows($resultado) > 0){
 
 			$_SESSION['user'] = $user;
 			$_SESSION['senha'] = $senha;
@@ -29,8 +31,30 @@
 
 			$logado = $_SESSION['user'];
 
-			header('Location:cadastro-escola.php');
+			header('Location:rel-est.php');
 			}
+
+			elseif(mysqli_num_rows($fim) > 0){
+
+				$_SESSION['user'] = $user;
+				$_SESSION['senha'] = $senha;
+
+				echo "  <script>
+						alert('Usuario\n".$_SESSION['user']."');
+					</script>";
+				if((!isset ($_SESSION['user']) == true) and (!isset ($_SESSION['senha']) == true))
+					{
+
+					unset($_SESSION['user']);
+					unset($_SESSION['senha']);
+
+				}
+
+				$logado = $_SESSION['user'];
+
+				header('Location:rel-escola.php');
+				}
+
 
 		else{
 		    unset($_SESSION['user']);
